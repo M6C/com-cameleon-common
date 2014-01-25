7f8118e58cabe117029d525596d2f3a396781b16
roca.david <roca.david@gmail.com>
2014-01-25 23:46:57 +0000

    Initial import.

---
+++ src/com/cameleon/common/android/db/sqlite/helper/DBAbstractHelper.java
@@ -0,0 +1,177 @@
+package com.cameleon.common.android.db.sqlite.helper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.Date;
+
+import org.gdocument.gtracergps.launcher.log.Logger;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
+
+import com.cameleon.common.android.inotifier.INotifierMessage;
+import com.cameleon.common.tool.ToolDatetime;
+
+public abstract class DBAbstractHelper extends SQLiteOpenHelper {
+
+	private static final String PACKAGE_NAME = "com.runningstars";
+	protected String databaseName;
+	protected int databaseVersion;
+	protected INotifierMessage notificationMessage;
+
+	public DBAbstractHelper(Context context, String databaseName, CursorFactory cursorFactory, int databaseVersion, INotifierMessage notificationMessage) {
+		super(context, databaseName, cursorFactory, databaseVersion);
+
+		this.databaseName = databaseName;
+		this.databaseVersion = databaseVersion;
+		this.notificationMessage = notificationMessage;
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		if (oldVersion != newVersion) {
+			logMe("Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+			try {
+				backupDbToSdcard(oldVersion);
+
+				logMe("Recreate database databaseName:" + databaseName);
+				recreateTable(database);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			logMe("No Upgrading database, identical version");
+		}
+	}
+
+	public void backupDbToSdcard() throws IOException {
+		backupDbToSdcard(databaseName);
+	}
+
+	public void backupDbToSdcard(int oldVersion) throws IOException {
+		String dbNameBackup = databaseName;
+		int dot = databaseName.lastIndexOf('.');
+		if (dot>0) {
+			dbNameBackup = databaseName.substring(0, dot) + "_" + 
+					oldVersion + "_" + 
+					ToolDatetime.toCompactedDatetime(new Date()) + "_" + 
+					databaseName.substring(dot);
+		}
+		else {
+			dbNameBackup = databaseName + "_" + 
+					oldVersion + "_" + 
+					ToolDatetime.toCompactedDatetime(new Date()) + "_" + 
+					".db";
+		}
+
+		logMe("Backup database databaseName:" + databaseName + " to " + dbNameBackup);
+		
+		backupDbToSdcard(dbNameBackup);
+	}
+	public void backupDbToSdcard(String database) throws IOException {
+		File sd = Environment.getExternalStorageDirectory();
+
+        if (sd.canWrite()) {
+    		String packageNames = getPackagename();
+            String backupDBPath = "/" + packageNames + "/" + database;
+
+            File dir = new File(sd, packageNames);
+        	if (!dir.exists() && !dir.mkdirs()) {
+        		throw new IOException("Dir '"+packageNames+"' creation error on sd");
+        	}
+
+	        File currentDB = new File(this.getWritableDatabase().getPath());
+	        File backupDB = new File(sd, backupDBPath);
+	
+	        copyDb(currentDB, backupDB);
+        }
+        else {
+        	notificationMessage.notifyMessage("sdcard directory is not writable");
+        }
+	}
+
+	public void restoreDbFromSdcard() throws IOException {
+		String packageNames = getPackagename();
+		String database = databaseName;
+        String backupDBPath = "/" + packageNames + "/" + database;
+
+        File sd = Environment.getExternalStorageDirectory();
+        File backupDB = new File(sd, backupDBPath);
+        if (sd.exists()) {
+
+        	if (sd.canRead()) {
+		        File currentDB = new File(this.getWritableDatabase().getPath());
+		        if (currentDB.canWrite()) {
+			        copyDb(backupDB, currentDB);
+		        }
+		        else {
+		        	notificationMessage.notifyMessage("current database is not writable");
+		        }
+            }
+            else {
+            	notificationMessage.notifyMessage("sdcard database can not be read");
+            }
+        }
+        else {
+        	notificationMessage.notifyMessage("sdcard database do not exist");
+        }
+	}
+	private void copyDb(File currentDB, File backupDB) {
+		try {
+            FileChannel src = null, dst = null;
+            try {
+                src = new FileInputStream(currentDB).getChannel();
+                dst = new FileOutputStream(backupDB).getChannel();
+                dst.transferFrom(src, 0, src.size());
+            }
+            finally {
+            	if (src!=null)
+            		src.close();
+            	if (dst!=null)
+            		dst.close();
+            }
+
+            notificationMessage.notifyMessage("copyDb From:" + currentDB.toString() + " To:" + backupDB.toString());
+	    } catch (Exception ex) {
+	    	notificationMessage.notifyError(ex);
+	    }
+	}
+
+	public void recreateTable(SQLiteDatabase database) {
+		dropTable(database);
+		onCreate(database);
+	}
+
+	private void dropTable(SQLiteDatabase database) {
+        notificationMessage.notifyMessage("drop database:" + database.getPath());
+
+        database.execSQL("DROP TABLE IF EXISTS " + getTableName());
+	}
+	
+	protected String getPackagename() {
+		return PACKAGE_NAME;
+	}
+
+	/**
+	 * Return the table name
+	 * @return
+	 */
+	protected abstract String getTableName();
+
+	/**
+	 * Return the class tag name for logs 
+	 * @return
+	 */
+	protected abstract String getTag();
+
+	protected void logMe(String msg) {
+		Logger.logMe(getTag(), msg);
+    }
+}
---
+++ src/com/cameleon/common/android/factory/FactoryDialog.java
@@ -0,0 +1,171 @@
+package com.cameleon.common.android.factory;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.cameleon.common.R;
+import com.cameleon.common.android.factory.listener.OnClickViewListener;
+
+public class FactoryDialog {
+
+	private static FactoryDialog instance;
+
+	private FactoryDialog() {
+	}
+
+	public static FactoryDialog getInstance() {
+		if (instance==null)
+			instance = new FactoryDialog();
+		return instance;
+	}
+
+	public Dialog buildListView(Context context, int titleId, String[] textList, final OnItemClickListener listener) {
+		//http://stackoverflow.com/questions/2874191/is-it-possible-to-create-listview-inside-dialog
+		//http://stackoverflow.com/questions/7302365/style-attributes-of-custom-styled-alertdialog
+		AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(context, R.style.AlertDialogTheme));
+
+		ListView modeList = new ListView(context);//new ContextThemeWrapper(context, R.style.AlertDialogList));
+		builder.setView(modeList);
+
+		View customTitle = View.inflate( context, R.layout.alert_dialog_title, null );
+		if (customTitle!=null) {
+			builder.setCustomTitle(customTitle);
+			TextView title = (TextView) customTitle.findViewById( R.id.alertTitle );
+			if (title!=null) {
+				title.setText(titleId);
+			}
+		}
+		
+		final Dialog dialog = builder.create();
+//		dialog.setTitle(titleId);
+
+		ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(new ContextThemeWrapper(context, R.style.AlertDialogItem), android.R.layout.simple_list_item_1, android.R.id.text1, textList);
+		modeList.setAdapter(modeAdapter);
+
+		modeList.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				listener.onItemClick(parent, view, position, id);
+				dialog.dismiss();
+			}
+		});
+
+		return dialog;
+	}
+
+	public void buildOkDialog(Context context, String title, String message) {
+//		AlertDialog.Builder builder = new Builder(context);
+		AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(context, R.style.AlertDialogTheme));
+		builder.setTitle(title);
+		builder.setMessage(message);
+		builder.setPositiveButton("OK", null);
+		builder.create().show();
+	}
+
+	public Dialog buildOkCancelDialog(Context context, OnClickListener onClickOkListener, int titleId, int messageId) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(context, R.style.AlertDialogTheme));
+		builder.setTitle(titleId);
+		builder.setMessage(messageId);
+		builder.setPositiveButton("OK", onClickOkListener);
+		builder.setNeutralButton("Cancel", null);
+		return builder.create();
+	}
+
+	public Dialog buildYesNoDialog(Context context, OnClickListener onClickListener, int titleId, int messageId) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(context, R.style.AlertDialogTheme));
+		builder.setTitle(titleId);
+		builder.setMessage(messageId);
+		builder.setNegativeButton("No", onClickListener);
+		builder.setPositiveButton("Yes", onClickListener);
+		return builder.create();
+	}
+
+		
+	public Dialog buildEditTextDialog(Context context, final OnClickViewListener onClickOkListener, int titleId, String text) {
+		return buildEditTextDialog(context, onClickOkListener, null, titleId, text);
+	}
+
+	public Dialog buildEditTextDialog(Context context, final OnClickViewListener onClickOkListener, final OnClickViewListener onClickCancelListener, int titleId, String text) {
+		LayoutInflater service = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View layout = service.inflate(R.layout.dialog_edit_text, null, true);
+		TextView textView = (TextView)layout.findViewById(R.id.tvText);
+		textView.setText(text);
+
+		return buildViewDialog(context, onClickOkListener, onClickCancelListener, layout, textView, titleId);
+	}
+
+	public Dialog buildDatePickerDialog(Context context, final OnClickViewListener onClickOkListener, int titleId, Date date) {
+		LayoutInflater service = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View layout = service.inflate(R.layout.date_picker, null, true);
+		DatePicker datePicker = (DatePicker)layout.findViewById(R.id.datePicker);
+
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(date);
+		datePicker.init(
+				calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+				null
+		);
+
+		return buildViewDialog(context, onClickOkListener, null, layout, datePicker, titleId);
+	}
+
+	public Dialog buildTimePickerDialog(Context context, final OnClickViewListener onClickOkListener, int titleId, Date date) {
+		LayoutInflater service = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View layout = service.inflate(R.layout.time_picker, null, true);
+		TimePicker timePicker = (TimePicker)layout.findViewById(R.id.timePicker);
+		timePicker.setIs24HourView(true);
+
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(date);
+		timePicker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
+		timePicker.setCurrentMinute(calendar.get(Calendar.MINUTE));
+
+		return buildViewDialog(context, onClickOkListener, null, layout, timePicker, titleId);
+	}
+
+	private Dialog buildViewDialog(Context context, final OnClickViewListener onClickOkListener, final OnClickViewListener onClickCancelListener, View layout, final View view, int titleId) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		if (titleId>0) {
+			builder.setTitle(titleId);
+		}
+		String txtNegative = null;
+		String txtPositive = null;
+		if (onClickCancelListener==null) {
+			txtNegative = "Cancel";
+			txtPositive = "Ok";
+		} else {
+			txtNegative = "No";
+			txtPositive = "Yes";
+		}
+		builder.setNegativeButton(txtNegative, (onClickCancelListener==null ? null : new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				onClickCancelListener.onClick(dialog, view, which);
+			}
+		}));
+		builder.setPositiveButton(txtPositive, new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				onClickOkListener.onClick(dialog, view, which);
+			}
+		});
+		builder.setView(layout);
+		return builder.create();
+	}
+}
