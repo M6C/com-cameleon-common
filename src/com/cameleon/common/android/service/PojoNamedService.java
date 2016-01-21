dd583d0142fdec77850c8730f95e39eb1f7b4d96
ckh146 <ckh146@775680ZN0213.dc.courrier.intra.laposte.fr>
2016-01-21 15:58:27 +0100

    Add - Method getNames withId possible in PojoNamedService

--- src/com/cameleon/common/android/service/PojoNamedService.java
+++ src/com/cameleon/common/android/service/PojoNamedService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import com.cameleon.common.android.model.GenericDBPojo;
+import com.cameleon.common.android.model.GenericDBPojoNamed;
 import com.cameleon.common.android.model.comparator.PojoNamedComparator;
 import com.cameleon.common.android.model.interfaces.IPojoNamed;
 
@@ -18,9 +20,17 @@ public class PojoNamedService {
 	}
 
 	public <P extends IPojoNamed> List<String> getNames(List<P> listPojo) {
+		return getNames(listPojo, false);
+	}
+
+	public <P extends IPojoNamed> List<String> getNames(List<P> listPojo, boolean withId) {
 		List<String> listTxtPojo = new ArrayList<String>();
 		for(IPojoNamed pojo : listPojo) {
-			listTxtPojo.add(pojo.getName());
+			String name = pojo.getName();
+			if (withId && pojo instanceof GenericDBPojoNamed) {
+				name = "[" + ((GenericDBPojo<?>)pojo).getId() + "] " + name;
+			}
+			listTxtPojo.add(name);
 		}
 		return listTxtPojo;
 	}
